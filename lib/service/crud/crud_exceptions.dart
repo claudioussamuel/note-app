f32869c7acce24d3b247c5575b6f26c20c94d88c
CLAUDIOUS OPOKU MENSAH <claudiouso00@gmail.com>
2024-04-13 22:38:02 +0000

    Step 11

---
+++ lib/service/crud/crud_exceptions.dart
@@ -0,0 +1,17 @@
+class DatabaseAlreadyOpenException implements Exception {}
+
+class UnableToGetDocumentDirectory implements Exception {}
+
+class CouldNotDeleteUser implements Exception {}
+
+class DatabaseIsNotOpen implements Exception {}
+
+class UserAlreadyExist implements Exception {}
+
+class CouldNotFindUser implements Exception {}
+
+class CouldNotDeleteNote implements Exception {}
+
+class CouldNotFindNotes implements Exception {}
+
+class CouldNotUpdateNotes implements Exception {}
